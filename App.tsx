b2face1cd86a80a85be460ff7c3b959ba5a1e91a
Miguel <migueljomartinez@gmail.com>
2023-07-25 22:50:10 -0500

    Finishing react-native-paper installation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {PaperProvider, Button} from 'react-native-paper';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -13,15 +14,23 @@ function App(): JSX.Element {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <View>
-          <Text style={styles.text}>Testing</Text>
-        </View>
-      </SafeAreaView>
+      <PaperProvider>
+        <SafeAreaView>
+          <StatusBar
+            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            backgroundColor={backgroundStyle.backgroundColor}
+          />
+          <View>
+            <Text style={styles.text}>Testing</Text>
+            <Button
+              icon="camera"
+              mode="contained"
+              onPress={() => console.log('Pressed')}>
+              Press me
+            </Button>
+          </View>
+        </SafeAreaView>
+      </PaperProvider>
     </SafeAreaProvider>
   );
 }
